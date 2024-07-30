d35ab13ccf10b9663c623a0ac747ee2ee88c22b7
Pfkshot <Pfkshot@gmail.com>
2024-07-30 14:13:59 +0300

    Sem3/1

---
+++ src/Sem3/Stream.java
@@ -0,0 +1,21 @@
+package Sem3;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Stream implements Iterable<StudentGroup>{
+    public List<StudentGroup> getStudentGroupList() {
+        return studentGroupList;
+    }
+
+    public void setStudentGroupList(List<StudentGroup> studentGroupList) {
+        this.studentGroupList = studentGroupList;
+    }
+
+    List<StudentGroup> studentGroupList;
+
+    @Override
+    public Iterator<StudentGroup> iterator() {
+        return new StreamIterator();
+    }
+}
